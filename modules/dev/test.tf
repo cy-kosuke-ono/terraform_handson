0a56c84fe822629b365d6317317db82c3898aafe
cy-kosuke-ono <kosuke_ono@cybird.co.jp>
2017-07-14 12:46:20 +0900

    Destroy EC2 instances

--- modules/dev/test.tf
+++ modules/dev/test.tf
@@ -1,18 +1,18 @@
-resource "aws_instance" "test" {
-  ami           = "ami-56d4ad31"
-  instance_type = "${var.instance_types["test"]}"
-  key_name      = "${var.uploaded_key}"
-
-  subnet_id                   = "${var.public_subnet_ids[0]}"
-  associate_public_ip_address = "false"
-
-  root_block_device = {
-    volume_type           = "gp2"
-    volume_size           = "15"
-    delete_on_termination = true
-  }
-
-  tags {
-    Name = "${var.env}-${var.app_name}-test"
-  }
-}
+# resource "aws_instance" "test" {
+#   ami           = "ami-56d4ad31"
+#   instance_type = "${var.instance_types["test"]}"
+#   key_name      = "${var.uploaded_key}"
+#
+#   subnet_id                   = "${var.public_subnet_ids[0]}"
+#   associate_public_ip_address = "false"
+#
+#   root_block_device = {
+#     volume_type           = "gp2"
+#     volume_size           = "15"
+#     delete_on_termination = true
+#   }
+#
+#   tags {
+#     Name = "${var.env}-${var.app_name}-test"
+#   }
+# }
