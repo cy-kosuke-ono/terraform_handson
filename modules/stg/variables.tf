8837888455b1006846247eb94bddbf5597f60aaf
cy-kosuke-ono <kosuke_ono@cybird.co.jp>
2017-07-14 12:00:53 +0900

    Append definision of variables

--- modules/stg/variables.tf
+++ modules/stg/variables.tf
@@ -1,1 +1,40 @@
 
+variable "env" {
+  type = "string"
+}
+
+variable "app_name" {
+  type = "string"
+}
+
+variable "instance_types" {
+  type = "map"
+}
+
+variable "instance_counts" {
+  type = "map"
+}
+
+variable "availability_zones" {
+  type = "list"
+}
+
+variable "vpc_id" {
+  type = "string"
+}
+
+variable "vpc_cidrs" {
+  type = "list"
+}
+
+variable "public_subnet_ids" {
+  type = "list"
+}
+
+variable "private_subnet_ids" {
+  type = "list"
+}
+
+variable "uploaded_key" {
+  type = "string"
+}
