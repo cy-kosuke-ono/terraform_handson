a5a27312d5cd63739ed8a2f454290b421104cddf
cy-kosuke-ono <kosuke_ono@cybird.co.jp>
2017-07-14 11:57:22 +0900

    Append variables in main.tf

--- envs/prd/main.tf
+++ envs/prd/main.tf
@@ -12,8 +12,30 @@ provider "aws" {
 
 module "common" {
   source = "../../modules/common/"
+
+  env                = "${var.env}"
+  app_name           = "${var.app_name}"
+  instance_types     = "${var.instance_types}"
+  instance_counts    = "${var.instance_counts}"
+  availability_zones = "${var.availability_zones}"
+  vpc_id             = "${var.vpc_id}"
+  vpc_cidrs          = "${var.vpc_cidrs}"
+  public_subnet_ids  = "${var.public_subnet_ids}"
+  private_subnet_ids = "${var.private_subnet_ids}"
+  uploaded_key       = "${var.uploaded_key}"
 }
 
 module "prd" {
   source = "../../modules/prd/"
+
+  env                = "${var.env}"
+  app_name           = "${var.app_name}"
+  instance_types     = "${var.instance_types}"
+  instance_counts    = "${var.instance_counts}"
+  availability_zones = "${var.availability_zones}"
+  vpc_id             = "${var.vpc_id}"
+  vpc_cidrs          = "${var.vpc_cidrs}"
+  public_subnet_ids  = "${var.public_subnet_ids}"
+  private_subnet_ids = "${var.private_subnet_ids}"
+  uploaded_key       = "${var.uploaded_key}"
 }
